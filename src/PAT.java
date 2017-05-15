6a4721f2474fbf39106adfaa4cb6069aa49e3557
José Aleixo <josealeixo.pc@gmail.com>
2017-05-15 07:00:37 +0100

    Deleted unnecessary code on PAT.

--- src/PAT.java
+++ src/PAT.java
@@ -28,11 +28,7 @@ import org.eclipse.jdt.core.dom.ASTParser;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import jsonParser.adapters.MyNodeDeserializer;
-import jsonParser.containers.BasicNode;
-import jsonParser.containers.Member;
-import jsonParser.containers.Reference;
-import jsonParser.containers.Root;
+import patternParser.BasicNode;
 import patternParser.BasicNode.Type;
 import patternsGrammar.ParseException;
 import patternsGrammar.Parser;
@@ -42,16 +38,6 @@ import utils.MyFileReader;
 public class PAT {
 
 	public static final String FS = System.getProperty("file.separator");
-	public static Map<patternParser.BasicNode.Type, Integer> typeToNodeType;
-	
-	private static void populateTypeToNodeType(){
-		
-		typeToNodeType = new HashMap<>();
-		
-		typeToNodeType.put(patternParser.BasicNode.Type.AssignmentExpression, ASTNode.VARIABLE_DECLARATION_FRAGMENT);
-		
-	}
-	
 
 	public static void main(String[] args) {
 
@@ -81,146 +67,20 @@ public class PAT {
 		CompilationUnit cu = null;
 		
 		try {
-			cu = eclipseAST(new String(Files.readAllBytes(Paths.get("/home/jazz/MyRepos/FEUP/3a-2s/feup-comp/input/" + JavaFilePath))).toCharArray());
+			cu = eclipseAST(new String(Files.readAllBytes(Paths.get("input" + FS + JavaFilePath))).toCharArray());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
+		BasicNode rootNode = getAST(root);
 		
-		populateTypeToNodeType();
-		
-		findPattern(cu, getAST(root));
-		
-		
-
-		// Generate AST from java code 
-		ProcessBuilder pb = new ProcessBuilder("java", "-jar", ".." + FS + "lib" + FS + "spoon2ast.jar", ".." + FS + "input" + FS + JavaFilePath)
-				.directory(new File("." + FS + "output"))
-				.inheritIO();
-		try {
-			Process p = pb.start();
-			p.waitFor();
-			p.getOutputStream();
-		} catch (IOException | InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return;
-		}
-
-		String JavaASTPath = "." + FS + "output" + FS + "ast.json";
-
-		File json = new File(JavaASTPath);
-		
-		MyNodeDeserializer typeAdapter = new MyNodeDeserializer();
-		
-		Gson gsonRead = new GsonBuilder()
-				.registerTypeAdapter(BasicNode.class, typeAdapter)
-				.registerTypeAdapter(Reference.class, typeAdapter)
-				.registerTypeAdapter(Expression.class, typeAdapter)
-				.registerTypeAdapter(Member.class, typeAdapter)
-				.setPrettyPrinting()
-				.create();
-				
-		Root jsonRootObject = gsonRead.fromJson(MyFileReader.read(json), Root.class);
-		
-		Gson gsonWrite = new GsonBuilder()
-				.setPrettyPrinting()
-				.disableHtmlEscaping()
-				.create();
-		System.out.println("Root: \n" +  gsonWrite.toJson(jsonRootObject));
-		
-		System.out.println(jsonRootObject.searchTreeForType(new ArrayList<>(), "Root"));
+		findPattern(cu, rootNode);
 	
-		if(true)
-		{
-			return;
-		}
+		System.out.println(rootNode);
 		
-
-		Map<String, Object> jsonJavaRootObject = gsonRead.fromJson(MyFileReader.read(json), Map.class);
-
-		//System.out.println("JSON:\n" + jsonJavaRootObject);
-
-		ArrayList<ArrayList<Map<String, Object>>> codeSections = getCodeSectionsFromRoot(jsonJavaRootObject);
-
-		for(ArrayList<Map<String, Object>> codeSection : codeSections){
-			for(Map<String, Object> statement : codeSection){
-				System.out.println(statement);
-			}
-			System.out.println();
-		}
-
-		System.out.println(root.jjtGetValue());
-		//PATTERNS
-		for(int i=0; i < root.jjtGetNumChildren(); i++){
-			((SimpleNode) root.jjtGetChild(i)).dump("");
-		}
-
-		//CICLO DE COMPARAR ASTS!
-		for(ArrayList<Map<String, Object>> codeSection : codeSections){
-			for(Map<String, Object> statement : codeSection){
-				if(sameType(statement.get("nodetype").toString(),root.jjtGetValue().toString())){
-					System.out.println("ENCONTREI UM IF");
-				}
-			}
-			System.out.println();
-		}
-
 	}
-
-	@SuppressWarnings("unchecked")
-	private static ArrayList<ArrayList<Map<String, Object>>> getCodeSectionsFromRoot(Map<String, Object> jsonJavaRootObject) {
-		ArrayList<ArrayList<Map<String, Object>>> statements = new ArrayList<ArrayList<Map<String, Object>>>();
-
-		//Compilation Units
-		ArrayList<Map<String, Object>> compilation_units = (ArrayList<Map<String, Object>>) jsonJavaRootObject.get("compilation_units");
-
-		//Types
-		ArrayList<Map<String, Object>> types = (ArrayList<Map<String, Object>>) compilation_units.get(0).get("types");
-
-		//Members
-		ArrayList<Map<String, Object>> members = (ArrayList<Map<String, Object>>) types.get(0).get("members");
-
-		//Bodies
-		ArrayList<Map<String, Object>> bodies = new ArrayList<Map<String, Object>>();
-		for(Map<String, Object> member : members){
-			bodies.add((Map<String, Object>) member.get("body"));
-		}
-
-		//Bodies
-		for(Map<String, Object> body : bodies){
-			statements.add((ArrayList<Map<String, Object>>) body.get("statements"));
-		}
-
-		return statements;
-	}
-
-	private static SimpleNode cleanRoot(SimpleNode root) {
-		SimpleNode cleanedRoot = new SimpleNode(0);
-
-		for(int i=0; i < root.jjtGetNumChildren(); i++){
-			if(((SimpleNode) root.jjtGetChild(i)).jjtGetValue() != null)
-				cleanedRoot.jjtAddChild(root.jjtGetChild(i),0);
-			cleanRoot((SimpleNode) root.jjtGetChild(i));
-		}
-
-		return cleanedRoot;
-	}
-
-
-
-
-
-	private static boolean sameType(String javaType, String dslType){
-		if(javaType.equals("If") && dslType.equals("if"))
-			return true;
-		else if(javaType.equals("LocalVariable") && dslType.equals("="))
-			return true;
-
-
-		return false;
-	}
+		
 	
 	private static CompilationUnit eclipseAST(char[] unit){
 		
@@ -240,7 +100,7 @@ public class PAT {
 		
 	}
 	
-	private static void findPattern(CompilationUnit cu, patternParser.BasicNode node){
+	private static void findPattern(CompilationUnit cu, BasicNode node){
 		
 		if(cu == null)
 		{
@@ -248,7 +108,7 @@ public class PAT {
 			return;
 		}
 		
-		patternParser.BasicNode.Type type = node.getType();
+		BasicNode.Type type = node.getType();
 		
 		MyASTVisitor visitor = new MyASTVisitor(node);
 		
@@ -259,84 +119,14 @@ public class PAT {
 			findPattern(cu, node.getChildren().get(i));
 		}
 		
+		
+		
 	}
 	
-	private static void findIf(CompilationUnit cu)
-	{
-		if(cu == null)
-		{
-			System.out.println("Given CompilationUnit is null.");
-			return;
-		}
 	
-
-		ASTVisitor visitor = (new ASTVisitor() {
-			 
-			/*public boolean visit(VariableDeclarationFragment node) {
-				SimpleName name = node.getName();
-				this.names.add(name.getIdentifier());
-				System.out.println("Declaration of '" + name + "' at line"
-						+ cu.getLineNumber(name.getStartPosition()));
-				return true; // do not continue 
-			}
-
-			public boolean visit(SimpleName node) {
-				if (this.names.contains(node.getIdentifier())) {
-					System.out.println("Usage of '" + node + "' at line "
-							+ cu.getLineNumber(node.getStartPosition()));
-				}
-				return true;
-			}*/
-			
-			/*
-			public boolean visit(MethodDeclaration node) {
-				SimpleName name = node.getName();
-				this.names.add(name.getIdentifier());
-				System.out.println("Declaration of '" + name + "' at line"
-						+ cu.getLineNumber(name.getStartPosition()));
-				return true; // do not continue 
-			}
-			
-			public boolean visit(ConditionalExpression node) {
-				Expression expr = node.getExpression();
-				this.names.add(expr.toString());
-				System.out.println("Declaration of '" + expr + "' at line"
-						+ cu.getLineNumber(expr.getStartPosition()));
-				return true; // do not continue 
-			}
-			
-			*/
-			
-			
-			
-			public boolean visit(IfStatement node) {
-				Expression expr = node.getExpression();
-				
-				expr.accept(new ASTVisitor(){
-					public boolean visit(InfixExpression node) {
-						
-						SimpleName simpleName = (SimpleName) node.getLeftOperand();
-						
-						if(simpleName.getIdentifier().equals("a"))
-						{
-							System.out.println("Found if at line " + cu.getLineNumber(simpleName.getStartPosition()));
-						}
-						
-							return true; // do not continue 
-					}
-				});
-				
-				return true;
-			}
-			
-		});
+	public static BasicNode getAST(SimpleNode node) {
 		
-		cu.accept(visitor);
-	}
-	
-	public static patternParser.BasicNode getAST(SimpleNode node) {
-		
-		patternParser.BasicNode ret = patternParser.BasicNode.parseFromString(node.toString());
+		BasicNode ret = BasicNode.parseFromString(node.toString());
 		
 		
 		for (int i = 0; i < node.jjtGetNumChildren(); i++)
--- src/patternParser/AssignmentExpression.java
+++ src/patternParser/AssignmentExpression.java
@@ -7,6 +7,8 @@ public class AssignmentExpression extends Expression{
 		
 	}
 
-	
-
+	@Override
+	public String toString() {
+		return "AssignmentExpression [type=" + type + ", value=" + value + ", children=" + children + "]";
+	}
 }
